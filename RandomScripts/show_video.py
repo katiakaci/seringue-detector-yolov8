4f5f19560ab3841e27c0c399d699d281a65f5867
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2025-03-24 16:22:00 -0400

    Photo de la seringue détectée par la webcam est enregistrée avec date et heure

--- RandomScripts/show_video.py
+++ RandomScripts/show_video.py
@@ -1,7 +1,14 @@
 import cv2
 from ultralytics import YOLO
+import os
+from datetime import datetime
 
 model = YOLO("C:/Users/katia/Desktop/GitHub/MTI805-Projet/runs/detect/train2/weights/best.pt") # Changer train2 par le modèle souhaité
+
+# Dossier pour sauvegarder les seringues détectées
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+SAVE_DIR = os.path.join(ROOT_DIR, "captures_seringues")
+os.makedirs(SAVE_DIR, exist_ok=True)
 
 cap = cv2.VideoCapture(0)
 
@@ -11,7 +18,22 @@ while cap.isOpened():
         break
     results = model(frame) # Détection
     annotated_frame = results[0].plot() # Annoter l'image
-    cv2.imshow("Détection de seringue", annotated_frame) # Afficher l'image
+
+    # Vérifier les détections
+    for box in results[0].boxes:
+        conf = box.conf[0].item()
+        if conf > 0.8:  # Seulement si > 80%
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            syringe_crop = frame[y1:y2, x1:x2]  # Recadrer la seringue
+
+            # Sauvegarde l'heure exacte à laquelle le médicament a été donné
+            timestamp = datetime.now().strftime("%d-%m-%Y_%Hh%Mm%Ss")
+            filename = os.path.join(SAVE_DIR, f"{timestamp}.jpg")
+            cv2.imwrite(filename, syringe_crop)
+            # print(f"Seringue sauvegardée : {filename}")
+
+    # Afficher l'image annotée
+    cv2.imshow("Détection de seringue", annotated_frame)
 
     # Quitter avec la touche 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
